1ac28846812cac842ae066590b02b5b0b17d9c3b
vinay <vk985815@gmail.com>
2025-01-22 12:01:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twiter", (req, res) => {
+  res.send("vinay kumar");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Please login at chai aur code</h1>");
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
